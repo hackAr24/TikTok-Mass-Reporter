6dc1455d081f6ac3faf976bb6641306ece0b4a6d
hackAr24 <jubayarislamsajid30@gmail.com>
2024-08-23 10:18:15 +0600

    Update start.bat
    {"status_message":"","data":{"need_captcha":false},"extra":{"now":1724386667000,"fatal_item_ids":[],"logid":"202408230417465ED1D8ED6A560B0A6184"},"log_pb":{"impr_id":"202408230417465ED1D8ED6A560B0A6184"},"status_code":0}

--- start.bat
+++ start.bat
@@ -1,2 +1,2 @@
 python main.py
-pause
+pause{"status_message":"","data":{"need_captcha":false},"extra":{"now":1724386667000,"fatal_item_ids":[],"logid":"202408230417465ED1D8ED6A560B0A6184"},"log_pb":{"impr_id":"202408230417465ED1D8ED6A560B0A6184"},"status_code":0}
